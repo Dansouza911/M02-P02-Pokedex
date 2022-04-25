1ce41188f58536ef611a3016f6b2d7ab05e2b206
Darlan Souza Santos Cruz <100162578+Dansouza911@users.noreply.github.com>
2022-04-25 20:52:26 -0300

    Projeto finazlizado

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -15,22 +16,35 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/detalhes/:id", (req, res) => {
+  let poke = [];
+  pokedex.filter((Element) => {
+    if (Element.id == req.params.id) poke.push(Element);
+  });
+  res.render("detalhes.ejs", {
+    poke,
+  });
+});
+app.get;
 
-app.get("/home", (req, res) => {
-  res.send("Olá mundo do express!");
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro.ejs");
 });
 
-app.get('/cadastro', (req,res) => {
-  res.render('cadastro.ejs')
-})
-
-app.post("/add", (req, res) => {
+app.post("/cadastro", (req, res) => {
   const pokemon = req.body;
   pokemon.id = pokedex.length + 1;
-pokedex.push(pokemon);
+  pokedex.push(pokemon);
   res.redirect("/");
-
 });
+
+/* app.post('/cadastro', (req, res) => {
+  let i = cavaleiros[cavaleiros.length-1].id + 1
+  const { nome, signos, tipo, iframe, img  } = req.body
+  cavaleiros.push({id: i, nome, signos, tipo, iframe, img})
+  console.log(cavaleiros)
+  res.redirect('/')
+}) */
 
 app.listen(3000, () =>
   console.log("Servidor rodando em http://localhost:3000")
@@ -41,56 +55,111 @@ let pokedex = [
     id: 1,
     nome: "Bulbasaur",
     tipo: "Grass",
-    imagem: "https://img.pokemondb.net/sprites/home/normal/bulbasaur.png",
+    descricao:
+      "Há uma semente de planta nas costas desde o dia em que este Pokémon nasce. A semente cresce lentamente.",
+
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    altura: "0,7m",
+    peso: "6,9kg",
+    categoria: "Semente",
+    habilidade: "Overgrow",
   },
   {
     id: 2,
     nome: "Charmander ",
     tipo: "Fire",
-    imagem: "https://img.pokemondb.net/sprites/home/normal/charmander.png",
+    descricao:
+      "Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta de sua cauda.",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    altura: "0,6m",
+    peso: "8,5kg",
+    categoria: "Lagarto",
+    habilidade: "Fire",
   },
+
   {
     id: 3,
     nome: "Squirtle  ",
     tipo: "Water",
+    descricao:
+      "Quando retrai seu longo pescoço em sua concha, esguicha água com força vigorosa.",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    altura: "0,5m",
+    peso: "9kg",
+    categoria: "Tartaruga",
+    habilidade: "Torrent",
   },
 
   {
     id: 4,
     nome: "Chikorita ",
     tipo: "Grass",
+    descricao:
+      "Na batalha, Chikorita agita sua folha para manter o inimigo afastado. No entanto, uma fragrância doce também emana da folha, acalmando o Pokémon em luta e criando uma atmosfera acolhedora e amigável ao redor.",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/152.png",
+    altura: "0,9m",
+    peso: "6,4kg",
+    categoria: "Folha",
+    habilidade: "Overgrow",
   },
   {
     id: 5,
     nome: "Cyndaquil  ",
     tipo: "Fire",
+    descricao:
+      "Cyndaquil se protege acendendo as chamas em suas costas. As chamas são vigorosas se o Pokémon estiver com raiva. No entanto, se estiver cansado, as chamas crepitam irregularmente com combustão incompleta. ",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png",
+    altura: "0,5m",
+    peso: "7,9kg",
+    categoria: "Rato de fogo",
+    habilidade: "Fire",
   },
   {
     id: 6,
     nome: "Totodile   ",
     tipo: "Water",
+    descricao:
+      "Apesar da pequenez de seu corpo, as mandíbulas de Totodile são muito poderosas. Embora o Pokémon possa pensar que está apenas beliscando de brincadeira, sua mordida tem poder suficiente para causar ferimentos graves.",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/158.png",
+    altura: "0,6m",
+    peso: "9,5kg",
+    categoria: "Mandíbula Grande",
+    habilidade: "Torrent",
   },
   {
     id: 7,
     nome: "Turtwig ",
     tipo: "Grass",
+    descricao:
+      "A fotossíntese ocorre em todo o seu corpo sob o sol. A concha em suas costas é, na verdade, solo endurecido.",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/387.png",
+    altura: "0,4m",
+    peso: "10,2kg",
+    categoria: "Folha",
+    habilidade: "Overgrow",
   },
   {
     id: 8,
     nome: "Chimchar  ",
     tipo: "Fire",
+    descricao:
+      "Sua extremidade traseira ardente é alimentada por gás feito em sua barriga. Nem a chuva pode apagar o fogo.",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/390.png",
+    altura: "0,5m",
+    peso: "6,2kg",
+    categoria: "Chimpanzé",
+    habilidade: "Fire",
   },
   {
     id: 9,
     nome: "Piplup   ",
     tipo: "Water",
+    descricao:
+      "Não gosta de ser cuidado. É difícil se relacionar com ele, pois não ouve seu treinador.",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/393.png",
+    altura: "0,4m",
+    peso: "5,2kg",
+    categoria: "Pinguin",
+    habilidade: "Torrent",
   },
-   
 ];
